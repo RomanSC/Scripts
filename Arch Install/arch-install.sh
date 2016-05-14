41aa564fda8b693fb6f4850a5ea809845c0cd051
RomanSC <r.s.col988@gmail.com>
2016-05-13 21:56:13 -0400

    Added pacman -Syy before pacstrap

--- Arch Install/arch-install.sh
+++ Arch Install/arch-install.sh
@@ -34,9 +34,10 @@ mount /dev/mapper/vol0-lv_root /mnt
 genfstab -U -p /mnt >> /mnt/etc/fstab
 
 
-#pacstrap
+#pacstrap uncomment xfce4 applications for xfce4 i3 hybrid Desktop
 echo "Installing the system: "
-pacstrap -i /mnt base base-devel intel-ucode networkmanager ufw openssh bash-completion bashdb mlocate zip dosfstools wget git rsync task mtpfs ruby python vim xfce4-session gtk-xfce4-engine xfce4-settings xfce4-panel xfce4-power-manager xfconf xfce4-artwork xfce4-battery-plugin xfce4-datetime-plugin xfce4-notifyd terminator i3-wm rofi compton nautilus dconf-editor redshift gnome-system-monitor gnome-calculator firefox gufw pidgin alsa-utils pulseaudio pavucontrol eog gimp inkscapegvfs-mtp gvfs-gphoto2 vlc libreoffice-still virtualbox virtualbox-host-dkms
+pacman -Syy
+pacstrap -i /mnt base base-devel intel-ucode networkmanager ufw openssh bash-completion bashdb mlocate zip dosfstools wget git rsync task mtpfs ruby python vim xfce4-notifyd terminator i3-wm rofi compton nautilus dconf-editor redshift gnome-system-monitor gnome-calculator firefox gufw pidgin alsa-utils pulseaudio pavucontrol eog gimp inkscapegvfs-mtp gvfs-gphoto2 vlc libreoffice-still virtualbox virtualbox-host-dkms # xfce4-session gtk-xfce4-engine xfce4-settings xfce4-panel xfce4-power-manager xfconf xfce4-artwork xfce4-battery-plugin xfce4-datetime-plugin
 
 #chroot
 arch-chroot /mnt /bin/bash/ -c "./chroot.sh"
